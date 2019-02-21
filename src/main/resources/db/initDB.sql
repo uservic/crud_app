f59008c3817d4938519cd46302773821e42be23e
uservic <vic1.wmail@gmail.com>
2019-02-21 16:15:19 +0300

    -- scripts for movieTheater db_schema created

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,16 +1,13 @@
--- drop table if exists user;
+drop table if exists orders;
+drop table if exists price_categories;
+drop table if exists seats;
+drop table if exists movie_sessions;
+drop table if exists hall;
+drop table if exists movies;
+
 drop table if exists users;
 drop table if exists dictionary;
 drop table if exists dt_seq_gen;
-
--- create table dictionary
--- (
--- 	id int auto_increment
--- 		primary key,
--- 	discriminator varchar(30) not null,
--- 	name varchar(255) not null,
--- 	ext_id int not null
--- );
 
 create table dictionary
 (
@@ -21,20 +18,6 @@ create table dictionary
 	name varchar(255) not null,
 	ext_id int not null
 );
-
--- create table user
--- (
--- 	id int auto_increment
--- 		primary key,
--- 	login varchar(31) not null,
--- 	password varchar(255) not null,
--- 	role int null,
--- 	constraint user_login_uindex
--- 		unique (login),
--- 	constraint user_role_fk
--- 		foreign key (role) references dictionary (id)
--- 			on delete set null
--- );
 
 create table users
 (
@@ -51,17 +34,6 @@ create table users
 			on delete set null
 );
 
--- create table dt_seq_gen
--- (
--- 	name varchar(255) not null,
--- 	counter int not null,
--- 	constraint `dict-types-seq-gen_name_uindex`
--- 		unique (name)
--- );
---
--- alter table dt_seq_gen
--- 	add primary key (name);
-
 create table dt_seq_gen
 (
 	name varchar(255)
@@ -69,3 +41,99 @@ create table dt_seq_gen
 			primary key,
 	counter int not null
 );
+
+/*Preliminary MovieTheatreTables initialization*/
+
+create table movies
+(
+  id serial not null
+    constraint movies_pk
+      primary key,
+  title varchar(31) not null,
+  description varchar(255)
+);
+
+alter table movies owner to postgres;
+
+create unique index movies_title_uindex
+  on movies (title);
+
+create table hall
+(
+  id serial not null
+    constraint hall_pk
+      primary key,
+  hall_name varchar(255) not null,
+  rows_quant integer not null,
+  seats_in_row integer not null
+);
+
+alter table hall owner to postgres;
+
+create table seats
+(
+  id serial not null
+    constraint seats_pk
+      primary key,
+  row integer not null,
+  number integer not null,
+  hall_id integer
+    constraint seats_hall_id_fk
+      references hall
+      on delete cascade
+);
+
+alter table seats owner to postgres;
+
+create table movie_sessions
+(
+  id serial not null
+    constraint movie_sessions_pk
+      primary key,
+  date date not null,
+  time time not null,
+  movie_id integer
+    constraint movie_sessions_movie_id_fk
+      references movies
+      on delete cascade,
+  hall_id integer
+    constraint movie_sessions_hall_id_fk
+      references hall
+);
+
+alter table movie_sessions owner to postgres;
+
+create table price_categories
+(
+  id serial not null
+    constraint price_categories_pk
+      primary key,
+  cat_type varchar(31) not null,
+  price integer not null
+);
+
+alter table price_categories owner to postgres;
+
+create unique index price_categories_cat_type_uindex
+  on price_categories (cat_type);
+
+create table orders
+(
+  id serial not null
+    constraint orders_pk
+      primary key,
+  price_cat_id integer not null
+    constraint orders_price_cat_id_fk
+      references price_categories,
+  user_id integer
+    constraint orders_user_id_fk
+      references users,
+  session_id integer not null
+    constraint orders_session_id_fk
+      references movie_sessions,
+  seat_id integer not null
+    constraint orders_seat_id_fk
+      references seats
+);
+
+alter table orders owner to postgres;
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,9 +1,13 @@
+delete from orders;
+delete from seats;
+delete from price_categories;
+delete from movie_sessions;
+delete from hall;
+delete from movies;
+
 delete from dictionary;
--- delete from user;
 delete from users;
 delete from dt_seq_gen;
--- ALTER TABLE dictionary AUTO_INCREMENT = 1;
--- ALTER TABLE user AUTO_INCREMENT = 1;
 ALTER SEQUENCE dictionary_id_seq RESTART WITH 1;
 ALTER SEQUENCE users_id_seq RESTART WITH 1;
 
@@ -19,3 +23,35 @@ insert into users (login, password, role) values
 
 insert into dt_seq_gen (name, counter) VALUES
 ('role', 2);
+
+
+insert into movies (id, title, description) values
+(1, 'Movie_Title_1', 'Description_1'),
+(2, 'Movie_Title_2', 'Description_2');
+
+insert into hall (id, hall_name, rows_quant, seats_in_row) values
+(1, 'Hall_A', 2, 2);
+
+insert into seats (id, row, number, hall_id) values
+(1, 1, 1, 1),
+(2, 1, 2, 1),
+(3, 2, 1, 1),
+(4, 2, 2, 1);
+
+insert into movie_sessions (id, date, time, movie_id, hall_id) VALUES
+(1, '2019-02-01', '12:00:00', 1, 1),
+(2, '2019-02-01', '15:00:00', 1, 1),
+(3, '2019-02-02', '22:00:00', 2, 1),
+(4, '2019-02-02', '23:30:00', 2, 1),
+(5, '2019-02-03', '09:00:00', 1, 1);
+
+insert into price_categories (id, cat_type, price) VALUES
+(1, 'morning', 150),
+(2, 'midday', 250),
+(3, 'afternoon', 350);
+
+insert into orders (id, price_cat_id, user_id, session_id, seat_id) VALUES
+(1, 2, 1, 1, 1),
+(2, 2, 1, 1, 2),
+(3, 3, 2, 2, 3),
+(4, 3, 3, 2, 4);
